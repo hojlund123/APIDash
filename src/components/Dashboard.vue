babaeb0a7d5038581a33791181b9f9c48e7f81de
Emil Højlund Larsen <hojlund123@users.noreply.github.com>
2024-06-06 14:13:23 +0200

    Create Dashboard.vue

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col>
+        <h1>Customizable Dashboard</h1>
+        <v-btn @click="addWidget">Add Widget</v-btn>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col v-for="widget in widgets" :key="widget.id">
+        <component :is="widget.component" :data="widget.data"></component>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const widgets = ref<{ id: number; component: string; data: any }[]>([])
+
+    const addWidget = () => {
+      widgets.value.push({
+        id: widgets.value.length,
+        component: 'ChartWidget', // This should match the component name for your widgets
+        data: {} // Placeholder for actual data
+      })
+    }
+
+    return { widgets, addWidget }
+  }
+})
+</script>
