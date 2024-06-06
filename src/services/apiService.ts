77a2f38024dc64937701fa58f47c4684b3ddc344
Emil Højlund Larsen <hojlund123@users.noreply.github.com>
2024-06-06 14:09:36 +0200

    Create apiService.ts

---
+++ src/services/apiService.ts
@@ -0,0 +1,14 @@
+// src/services/apiService.ts
+import axios from 'axios'
+
+const fetchData = async (url: string) => {
+  try {
+    const response = await axios.get(url)
+    return response.data
+  } catch (error) {
+    console.error('Error fetching data:', error)
+    throw error
+  }
+}
+
+export { fetchData }
